dfdca6e29ccf1e63bffbd73fef18f8fc6bd97830
lolly12345 <95150587+lolly12345@users.noreply.github.com>
2022-03-02 16:41:44 +1100

    Add files via upload

---
+++ MatterJSBoilerPlate-main/MatterJSBoilerPlate-main/ground.js
@@ -0,0 +1,16 @@
+class Ground 
+{
+  constructor(x, y, w, h) 
+  {
+    let options = {
+      isStatic:true
+    };
+    
+ leftSide(this.x,this.y,this.w,this.h)
+ rightSide(this.x,this.y,this.w,this.h)
+  }
+  
+}
+
+
+ground.Obj.display()
